046b46c83781ecdee20aa214575456393fb002ee
ljjsx <95336780+ljjsx@users.noreply.github.com>
2024-06-11 16:56:22 +0800

    Add files via upload

---
+++ douzero/dmc/models.py
@@ -0,0 +1,370 @@
+"""
+This file includes the torch models. We wrap the three
+models into one class for convenience.
+"""
+
+import numpy as np
+
+import torch
+from torch import nn
+
+class LandlordLstmModel_big(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(162, 128, batch_first=True)
+        self.dense1 = nn.Linear(373 + 128, 512)
+        self.dense2 = nn.Linear(512, 512)
+        self.dense3 = nn.Linear(512, 512)
+        self.dense4 = nn.Linear(512, 512)
+        self.dense5 = nn.Linear(512, 512)
+        self.dense6 = nn.Linear(512, 512)
+        self.dense7 = nn.Linear(512, 512)
+        self.dense8 = nn.Linear(512, 1)
+
+    def forward(self, z, x, return_value=False, flags=None):
+        lstm_out, (h_n, _) = self.lstm(z)
+        lstm_out = lstm_out[:,-1,:]
+        x = torch.cat([lstm_out,x], dim=-1)
+        x = self.dense1(x)
+        x = torch.relu(x)
+        x = self.dense2(x)
+        x = torch.relu(x)
+        x = self.dense3(x)
+        x = torch.relu(x)
+        x = self.dense4(x)
+        x = torch.relu(x)
+        x = self.dense5(x)
+        x = torch.relu(x)
+        x = self.dense6(x)
+        x = torch.relu(x)
+        x = self.dense7(x)
+        x = torch.relu(x)
+        x = self.dense8(x)
+        if return_value:
+            return dict(values=x)
+        else:
+            if flags is not None and flags.exp_epsilon > 0 and np.random.rand() < flags.exp_epsilon:
+                action = torch.randint(x.shape[0], (1,))[0]
+            else:
+                action = torch.argmax(x,dim=0)[0]
+            return dict(action=action)
+
+class LandlordLstmModel_small(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(162, 128, batch_first=True)
+        self.dense1 = nn.Linear(373 + 128, 512)
+        self.dense2 = nn.Linear(512, 512)
+        self.dense3 = nn.Linear(512, 512)
+        self.dense6 = nn.Linear(512, 1)
+
+    def forward(self, z, x, return_value=False, flags=None):
+        lstm_out, (h_n, _) = self.lstm(z)
+        lstm_out = lstm_out[:,-1,:]
+        x = torch.cat([lstm_out,x], dim=-1)
+        x = self.dense1(x)
+        x = torch.relu(x)
+        x = self.dense2(x)
+        x = torch.relu(x)
+        x = self.dense3(x)
+        x = torch.relu(x)
+        x = self.dense6(x)
+        if return_value:
+            return dict(values=x)
+        else:
+            if flags is not None and flags.exp_epsilon > 0 and np.random.rand() < flags.exp_epsilon:
+                action = torch.randint(x.shape[0], (1,))[0]
+            else:
+                action = torch.argmax(x,dim=0)[0]
+            return dict(action=action)
+
+class LandlordLstmModel_res(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(162, 128, batch_first=True)
+        self.dense1 = nn.Linear(373 + 128, 512)
+        self.dense2 = nn.Linear(512, 512)
+        self.dense3 = nn.Linear(512, 512)
+        self.dense4 = nn.Linear(512, 512)
+        self.dense5 = nn.Linear(512, 512)
+        self.dense6 = nn.Linear(512, 1)
+
+    def forward(self, z, x, return_value=False, flags=None):
+        lstm_out, (h_n, _) = self.lstm(z)
+        lstm_out = lstm_out[:,-1,:]
+        x = torch.cat([lstm_out,x], dim=-1)
+        x = self.dense1(x)
+        x = torch.relu(x)
+        res = x
+        x = self.dense2(x)
+        x = torch.relu(x)
+        x = self.dense3(x)
+        x = torch.relu(x)
+        x = self.dense4(x)
+        x = torch.relu(x)
+        x = self.dense5(x)
+        x = torch.relu(x)
+        x = x + res
+        x = self.dense6(x)
+        if return_value:
+            return dict(values=x)
+        else:
+            if flags is not None and flags.exp_epsilon > 0 and np.random.rand() < flags.exp_epsilon:
+                action = torch.randint(x.shape[0], (1,))[0]
+            else:
+                action = torch.argmax(x,dim=0)[0]
+            return dict(action=action)
+
+class LandlordLstmModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(162, 128, batch_first=True)
+        self.dense1 = nn.Linear(373 + 128, 512)
+        self.dense2 = nn.Linear(512, 512)
+        self.dense3 = nn.Linear(512, 512)
+        self.dense4 = nn.Linear(512, 512)
+        self.dense5 = nn.Linear(512, 512)
+        self.dense6 = nn.Linear(512, 1)
+
+    def forward(self, z, x, return_value=False, flags=None):
+        lstm_out, (h_n, _) = self.lstm(z)
+        lstm_out = lstm_out[:,-1,:]
+        x = torch.cat([lstm_out,x], dim=-1)
+        x = self.dense1(x)
+        x = torch.relu(x)
+        x = self.dense2(x)
+        x = torch.relu(x)
+        x = self.dense3(x)
+        x = torch.relu(x)
+        x = self.dense4(x)
+        x = torch.relu(x)
+        x = self.dense5(x)
+        x = torch.relu(x)
+        x = self.dense6(x)
+        if return_value:
+            return dict(values=x)
+        else:
+            if flags is not None and flags.exp_epsilon > 0 and np.random.rand() < flags.exp_epsilon:
+                action = torch.randint(x.shape[0], (1,))[0]
+            else:
+                action = torch.argmax(x,dim=0)[0]
+            return dict(action=action)
+
+class FarmerLstmModel_big(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(162, 128, batch_first=True)
+        self.dense1 = nn.Linear(484 + 128, 512)
+        self.dense2 = nn.Linear(512, 512)
+        self.dense3 = nn.Linear(512, 512)
+        self.dense4 = nn.Linear(512, 512)
+        self.dense5 = nn.Linear(512, 512)
+        self.dense6 = nn.Linear(512, 512)
+        self.dense7 = nn.Linear(512, 512)
+        self.dense8 = nn.Linear(512, 1)
+
+    def forward(self, z, x, return_value=False, flags=None):
+        lstm_out, (h_n, _) = self.lstm(z)
+        lstm_out = lstm_out[:,-1,:]
+        x = torch.cat([lstm_out,x], dim=-1)
+        x = self.dense1(x)
+        x = torch.relu(x)
+        x = self.dense2(x)
+        x = torch.relu(x)
+        x = self.dense3(x)
+        x = torch.relu(x)
+        x = self.dense4(x)
+        x = torch.relu(x)
+        x = self.dense5(x)
+        x = torch.relu(x)
+        x = self.dense6(x)
+        x = torch.relu(x)
+        x = self.dense7(x)
+        x = torch.relu(x)
+        x = self.dense8(x)
+        if return_value:
+            return dict(values=x)
+        else:
+            if flags is not None and flags.exp_epsilon > 0 and np.random.rand() < flags.exp_epsilon:
+                action = torch.randint(x.shape[0], (1,))[0]
+            else:
+                action = torch.argmax(x,dim=0)[0]
+            return dict(action=action)
+
+class FarmerLstmModel_small(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(162, 128, batch_first=True)
+        self.dense1 = nn.Linear(484 + 128, 512)
+        self.dense2 = nn.Linear(512, 512)
+        self.dense3 = nn.Linear(512, 512)
+        self.dense6 = nn.Linear(512, 1)
+
+    def forward(self, z, x, return_value=False, flags=None):
+        lstm_out, (h_n, _) = self.lstm(z)
+        lstm_out = lstm_out[:,-1,:]
+        x = torch.cat([lstm_out,x], dim=-1)
+        x = self.dense1(x)
+        x = torch.relu(x)
+        x = self.dense2(x)
+        x = torch.relu(x)
+        x = self.dense3(x)
+        x = torch.relu(x)
+        x = self.dense6(x)
+        if return_value:
+            return dict(values=x)
+        else:
+            if flags is not None and flags.exp_epsilon > 0 and np.random.rand() < flags.exp_epsilon:
+                action = torch.randint(x.shape[0], (1,))[0]
+            else:
+                action = torch.argmax(x,dim=0)[0]
+            return dict(action=action)
+
+class FarmerLstmModel_res(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(162, 128, batch_first=True)
+        self.dense1 = nn.Linear(484 + 128, 512)
+        self.dense2 = nn.Linear(512, 512)
+        self.dense3 = nn.Linear(512, 512)
+        self.dense4 = nn.Linear(512, 512)
+        self.dense5 = nn.Linear(512, 512)
+        self.dense6 = nn.Linear(512, 1)
+
+    def forward(self, z, x, return_value=False, flags=None):
+        lstm_out, (h_n, _) = self.lstm(z)
+        lstm_out = lstm_out[:,-1,:]
+        x = torch.cat([lstm_out,x], dim=-1)
+        x = self.dense1(x)
+        x = torch.relu(x)
+        res = x
+        x = self.dense2(x)
+        x = torch.relu(x)
+        x = self.dense3(x)
+        x = torch.relu(x)
+        x = self.dense4(x)
+        x = torch.relu(x)
+        x = self.dense5(x)
+        x = torch.relu(x)
+        x = x + res
+        x = self.dense6(x)
+        if return_value:
+            return dict(values=x)
+        else:
+            if flags is not None and flags.exp_epsilon > 0 and np.random.rand() < flags.exp_epsilon:
+                action = torch.randint(x.shape[0], (1,))[0]
+            else:
+                action = torch.argmax(x,dim=0)[0]
+            return dict(action=action)
+
+class FarmerLstmModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.lstm = nn.LSTM(162, 128, batch_first=True)
+        self.dense1 = nn.Linear(484 + 128, 512)
+        self.dense2 = nn.Linear(512, 512)
+        self.dense3 = nn.Linear(512, 512)
+        self.dense4 = nn.Linear(512, 512)
+        self.dense5 = nn.Linear(512, 512)
+        self.dense6 = nn.Linear(512, 1)
+
+    def forward(self, z, x, return_value=False, flags=None):
+        lstm_out, (h_n, _) = self.lstm(z)
+        lstm_out = lstm_out[:,-1,:]
+        x = torch.cat([lstm_out,x], dim=-1)
+        x = self.dense1(x)
+        x = torch.relu(x)
+        x = self.dense2(x)
+        x = torch.relu(x)
+        x = self.dense3(x)
+        x = torch.relu(x)
+        x = self.dense4(x)
+        x = torch.relu(x)
+        x = self.dense5(x)
+        x = torch.relu(x)
+        x = self.dense6(x)
+        if return_value:
+            return dict(values=x)
+        else:
+            if flags is not None and flags.exp_epsilon > 0 and np.random.rand() < flags.exp_epsilon:
+                action = torch.randint(x.shape[0], (1,))[0]
+            else:
+                action = torch.argmax(x,dim=0)[0]
+            return dict(action=action)
+
+
+
+# Model dict is only used in evaluation but not training
+model_dict = {"small":{}, 'big':{},'base':{},'res':{}, 'douzero_WP':{}}
+model_dict['res']['landlord'] = LandlordLstmModel_res
+model_dict['res']['landlord_up'] = FarmerLstmModel_res
+model_dict['res']['landlord_down'] = FarmerLstmModel_res
+
+model_dict['small']['landlord'] = LandlordLstmModel_small
+model_dict['small']['landlord_up'] = FarmerLstmModel_small
+model_dict['small']['landlord_down'] = FarmerLstmModel_small
+
+model_dict['big']['landlord'] = LandlordLstmModel_big
+model_dict['big']['landlord_up'] = FarmerLstmModel_big
+model_dict['big']['landlord_down'] = FarmerLstmModel_big
+
+model_dict['base']['landlord'] = LandlordLstmModel
+model_dict['base']['landlord_up'] = FarmerLstmModel
+model_dict['base']['landlord_down'] = FarmerLstmModel
+
+model_dict['douzero_WP']['landlord'] = LandlordLstmModel
+model_dict['douzero_WP']['landlord_up'] = FarmerLstmModel
+model_dict['douzero_WP']['landlord_down'] = FarmerLstmModel
+
+class Model:
+    """
+    The wrapper for the three models. We also wrap several
+    interfaces such as share_memory, eval, etc.
+    """
+    def __init__(self, device=0, version='base'):
+        self.models = {}
+        if not device == "cpu":
+            device = 'cuda:' + str(device)
+        if version == 'base':
+            self.models['landlord'] = LandlordLstmModel().to(torch.device(device))
+            self.models['landlord_up'] = FarmerLstmModel().to(torch.device(device))
+            self.models['landlord_down'] = FarmerLstmModel().to(torch.device(device))
+            print(f"model:base")
+        elif version == 'res':
+            self.models['landlord'] = LandlordLstmModel_res().to(torch.device(device))
+            self.models['landlord_up'] = FarmerLstmModel_res().to(torch.device(device))
+            self.models['landlord_down'] = FarmerLstmModel_res().to(torch.device(device))
+            print(f"model:res")
+        elif version == 'small':
+            self.models['landlord'] = LandlordLstmModel_small().to(torch.device(device))
+            self.models['landlord_up'] = FarmerLstmModel_small().to(torch.device(device))
+            self.models['landlord_down'] = FarmerLstmModel_small().to(torch.device(device))
+            print(f"model:small")
+        elif version == 'big':
+            self.models['landlord'] = LandlordLstmModel_big().to(torch.device(device))
+            self.models['landlord_up'] = FarmerLstmModel_big().to(torch.device(device))
+            self.models['landlord_down'] = FarmerLstmModel_big().to(torch.device(device))
+            print(f"model:big")
+
+    def forward(self, position, z, x, training=False, flags=None):
+        model = self.models[position]
+        return model.forward(z, x, training, flags)
+
+    def share_memory(self):
+        self.models['landlord'].share_memory()
+        self.models['landlord_up'].share_memory()
+        self.models['landlord_down'].share_memory()
+
+    def eval(self):
+        self.models['landlord'].eval()
+        self.models['landlord_up'].eval()
+        self.models['landlord_down'].eval()
+
+    def parameters(self, position):
+        return self.models[position].parameters()
+
+    def get_model(self, position):
+        return self.models[position]
+
+    def get_models(self):
+        return self.models
